f0955f4b87cf239c692d599ace28fc1f66b95c2f
email <email@email.fr>
2017-06-14 14:52:50 +0200

    ajout du fichier erreur

--- Web/Index.php
+++ Web/Index.php
@@ -5,6 +5,13 @@ $controllerName = $_GET["controller"];
 //Ici le dossier WEB
 define('ROOT_PATH', dirname(__DIR__));
 
-//Exécution du controller
+//Définition du chemin du controller
 require ROOT_PATH.'/src/controllers/'.$controllerName.'.php';
+//Test de l'existence du controller
+if (! file_exists($controllerPath)) {
+	//Envoie vers le fichier erreur
+	$controllerPath = ROOT_PATH.'/src/controllers/erreur.php';
+}
+//Execution du controlleur
+require $controllerPath;
 ?>
