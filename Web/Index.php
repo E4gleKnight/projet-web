e0f2ad291fa6c44922580be8b6ab62600d41ca99
email <email@email.fr>
2017-06-14 15:06:58 +0200

    page accueil par défaut

--- Web/Index.php
+++ Web/Index.php
@@ -1,12 +1,18 @@
 <?php
 //Récupération du controller
-$controllerName = $_GET["controller"];
+//avec gestion de la page de défaut
+if (isset($_GET["controller"])) {
+	$controllerName = $_GET["controller"];
+}else{
+	$controllerName = "accueil";
+}
+
 //Définition du dossier racine du projet
 //Ici le dossier WEB
 define('ROOT_PATH', dirname(__DIR__));
 
 //Définition du chemin du controller
-require ROOT_PATH.'/src/controllers/'.$controllerName.'.php';
+$controllerPath = ROOT_PATH.'/src/controllers/'.$controllerName.'.php';
 //Test de l'existence du controller
 if (! file_exists($controllerPath)) {
 	//Envoie vers le fichier erreur
